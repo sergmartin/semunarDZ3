fb220bc0cdc6372b3d3a15d01d0d6a02a8a4e0b6
Сергей <sergemartin@yandex.ru>
2022-09-30 00:08:05 +0300

    сделал задачу №21

---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №21
+Console.Write("Введите значение x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double r = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+Console.Write($"Расстояние между точками = {r} ");
