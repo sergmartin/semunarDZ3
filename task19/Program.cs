18c59641a01daef7990fa4f9c328a91935c61e17
Сергей <sergemartin@yandex.ru>
2022-09-29 23:04:40 +0300

    создал задачу 19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 19
+
+int n = 0;
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+string m = Convert.ToString(n);
+ 
+ if (m[0] == m[4] && m[1] == m[3]) Console.Write($" {n} полиндром!");
+ else Console.Write($" {n} не полиндром!");
+ 
