c0057567500ee71732d2717c37d86adc9d866a3e
Сергей <sergemartin@yandex.ru>
2022-09-29 23:19:28 +0300

    Задача №23

---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №23
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+if (n != 0)
+{
+    for (int i = 1; i < n; i++) 
+    {
+        m = n*n*n;
+        
+    
+    }
+    Console.Write(m);
+}
+else Console.Write("ноль использовать нельзя!");
